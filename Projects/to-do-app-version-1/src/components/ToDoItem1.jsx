eb340cbfcbba6e1fdc204f8a1da50315503d2380
akshita1204 <akshitatyagi04@gmail.com>
2024-11-23 15:18:49 +0530

    Basic ToDo App UI

---
+++ Projects/to-do-app-version-1/src/components/ToDoItem1.jsx
@@ -0,0 +1,19 @@
+function ToDoItem1()
+{
+return(
+    <div class="container">
+    <div class="row kg-row">
+    <div class="col-6">
+    Purchase Books
+    </div>
+ <div class="col-4">
+    23/11/2024
+   </div>
+ <div class="col-2">
+ <button type="button" class="btn btn-danger kg-button">Delete</button>
+ </div>
+</div>
+</div>
+);
+}
+export default ToDoItem1;
