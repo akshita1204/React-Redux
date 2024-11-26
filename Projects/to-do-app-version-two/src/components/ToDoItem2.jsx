5e1dabecaff070eca1f2fe6fbdfd14f4bbd4033e
akshita1204 <akshitatyagi04@gmail.com>
2024-11-26 16:08:55 +0530

    commit changes

---
+++ Projects/to-do-app-version-two/src/components/ToDoItem2.jsx
@@ -0,0 +1,19 @@
+function ToDoItem2()
+{
+return(
+    <div class="container">
+    <div class="row kg-row">
+    <div class="col-6">
+    Prepare for Exam
+    </div>
+ <div class="col-4">
+    24/11/2024
+   </div>
+ <div class="col-2">
+ <button type="button" class="btn btn-danger kg-button">Delete</button>
+ </div>
+</div>
+</div>
+);
+}
+export default ToDoItem2;
