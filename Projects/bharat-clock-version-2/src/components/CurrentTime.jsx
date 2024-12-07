13ce90374a14a4c99ec70623267b3f36eecad60a
akshita1204 <akshitatyagi04@gmail.com>
2024-12-07 10:57:24 +0530

    commit changes

--- Projects/bharat-clock-version-2/src/components/CurrentTime.jsx
+++ Projects/bharat-clock-version-2/src/components/CurrentTime.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
 let CurrentTime=()=>
     {
-        let time=new Date();
+      const[time,settime]=useState(new Date());
+      useEffect(()=>{
+        const intervalId=setInterval(()=>
+        {
+          settime(new Date());
+        },1000);
+        
+        return()=> {
+          clearInterval(intervalId);
+        }
+      }
+    ,[])
+        // let time=new Date();
       return <p className="lead">This is the current time:{time.toLocaleDateString()}-{time.toLocaleTimeString()}</p>
     };
     export default CurrentTime;
